8f132ebb0876dca6348835eceaf9697e13e0db48
beepscore <support@beepscore.com>
2010-03-25 23:23:46 -0700

    Add class DrawStuffView. In .xib, change view class from UIView to DrawStuffView.

---
+++ Classes/DrawStuffView.h
@@ -0,0 +1,16 @@
+//
+//  DrawStuffView.h
+//  DrawStuff
+//
+//  Created by Steve Baker on 3/25/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DrawStuffView : UIView {
+
+}
+
+@end
---
+++ Classes/DrawStuffView.m
@@ -0,0 +1,33 @@
+//
+//  DrawStuffView.m
+//  DrawStuff
+//
+//  Created by Steve Baker on 3/25/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "DrawStuffView.h"
+
+
+@implementation DrawStuffView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
