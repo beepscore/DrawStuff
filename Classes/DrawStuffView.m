c7e5bf65d49dbdae5ffd476257efceff3e20631d
beepscore <support@beepscore.com>
2010-03-26 00:09:29 -0700

    Add drawing method. Adjust dimensions for phone window portrait orientation.
    Note:  Including math.h may not be necessary.  I think M_PI may already be defined.

--- Classes/DrawStuffView.m
+++ Classes/DrawStuffView.m
@@ -7,6 +7,7 @@
 //
 
 #import "DrawStuffView.h"
+#include <math.h>     // for M_PI
 
 
 @implementation DrawStuffView
@@ -20,14 +21,54 @@
 }
 
 
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-
-
+#pragma mark Memory management methods
 - (void)dealloc {
     [super dealloc];
 }
 
+
+void doClippedCircle(CGContextRef context)
+{
+    // adjust dimensions for phone window portrait orientation
+    CGPoint circleCenter = {160., 100.};
+    float circleRadius = 80.0;
+    float startingAngle = 0.0, endingAngle = 2*M_PI;
+    CGRect ourRect = { {90.,30.} , {140., 140.} };
+    
+    // ***** Filled Circle *****
+    CGContextSetRGBFillColor(context, 0.663, 0., 0.031, 1.0);
+    CGContextBeginPath(context);
+    // Construct the circle path counterclockwise.
+    CGContextAddArc(context, circleCenter.x, circleCenter.y, circleRadius,
+                    startingAngle, endingAngle, 0);
+    CGContextDrawPath(context, kCGPathFill);
+    
+    // ***** Stroked Square *****
+    CGContextStrokeRect(context, ourRect);
+    
+    // Translate so that the next drawing doesn't overlap what
+    // has already been drawn.
+//    CGContextTranslateCTM(context,
+//                          ourRect.size.width + circleRadius + 5., 0);
+    CGContextTranslateCTM(context,
+                          0, ourRect.size.height + circleRadius + 5.0);
+    // Create a rectangular path and clip to that path.
+    CGContextBeginPath(context);
+    CGContextAddRect(context, ourRect);
+    CGContextClip(context);
+    
+    // ***** Clipped Circle *****
+    CGContextBeginPath(context);
+    // Construct the circle path counterclockwise.
+    CGContextAddArc(context, circleCenter.x, circleCenter.y, circleRadius,
+                    startingAngle, endingAngle, 0);
+    CGContextDrawPath(context, kCGPathFill);
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    doClippedCircle(context);
+}
 
 @end
